cbd24cf1ec3c5096bc328c67e4f7c3bf3056db62
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2018-06-08 10:43:07 -0700

    add several object to root directory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { News } from './models/news.model';
+import { Article } from './models/article.model';
+import { Section } from './models/section.model';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +14,43 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
+
+
+  selectedSection = null;
+
+  masterSectionList: Section[] = [
+    new Section('World',
+          [
+              new Article('Singapoure Summit', 'Donec at orci tincidunt dolor mattis rutrum sit amet eget lectus. Aliquam erat volutpat.', 2018),
+              new Article('Meeting in France', 'Etiam et pretium magna. Sed in dui eu magna dapibus sollicitudin vel accumsan magna', 2017),
+            ]),
+    new Section('Business',
+          [
+              new Article('Google stocks', 'Donec at orci tincidunt dolor mattis rutrum sit amet eget lectus. Aliquam erat volutpat.', 2018),
+              new Article('Github acquiring', 'Etiam et pretium magna. Sed in dui eu magna dapibus sollicitudin vel accumsan magna', 2017)
+            ]),
+    new Section('Politics',
+          [
+              new Article('Republicans', 'Donec at orci tincidunt dolor mattis rutrum sit amet eget lectus. Aliquam erat volutpat.', 2018),
+              new Article('Democrats', 'Etiam et pretium magna. Sed in dui eu magna dapibus sollicitudin vel accumsan magna', 2017)
+            ]),
+    new Section('Sport',
+          [
+              new Article('Argentina', 'World Cup 2018 champions.', 2018),
+              new Article('NBA', 'Champion of NBA finals 2018', 2018)
+            ])
+  ];
+
+  editSection(clickedSection) {
+    this.selectedSection = clickedSection;
+  }
+
+  finishedEditing() {
+    this.selectedSection = null;
+  }
+
+  addSection(newSection: Section) {
+    this.masterSectionList.push(newSection);
+  }
+
 }
