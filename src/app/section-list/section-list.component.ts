713dfd114369c85076dd2b519616594ee8cfc057
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2018-06-08 13:20:59 -0700

    add class of SectionList

--- src/app/section-list/section-list.component.ts
+++ src/app/section-list/section-list.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Section } from '../models/section.model';
 
 @Component({
   selector: 'app-section-list',
   templateUrl: './section-list.component.html',
   styleUrls: ['./section-list.component.css']
 })
-export class SectionListComponent implements OnInit {
+export class SectionListComponent {
+  @Input() childSectionList: Section[];
+  @Output() clickSender = new EventEmitter();
 
-  constructor() { }
+
 
   ngOnInit() {
   }
