7f3c06738838ad41e6724ac3daaecfd088abcf56
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2018-06-15 15:17:22 -0700

    add funcionality to article list component

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../article.model';
+import { Router } from '@angular/router';
+import { ArticleService } from '../article.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-article-list',
   templateUrl: './article-list.component.html',
-  styleUrls: ['./article-list.component.css']
+  styleUrls: ['./article-list.component.css'],
+  providers: [ArticleService]
 })
 export class ArticleListComponent implements OnInit {
+  albums: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
-  constructor() { }
+  constructor(private router: Router, private articleService: ArticleService) { }
 
   ngOnInit() {
+    this.articles = this.articleService.getArticles();
   }
 
+  goToDetailPage(clickedArticle) {
+    this.router.navigate(['articles', clickedArticle.$key]);
+  }
 }
