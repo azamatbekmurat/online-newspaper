445217be94967c953342c009a0ec12eeeca6ebc6
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2018-06-15 15:16:42 -0700

    add functionality to detail component

--- src/app/article-detail/article-detail.component.ts
+++ src/app/article-detail/article-detail.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Article } from '../article.model';
+import { ArticleService } from '../article.service';
+import { FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-article-detail',
   templateUrl: './article-detail.component.html',
-  styleUrls: ['./article-detail.component.css']
+  styleUrls: ['./article-detail.component.css'],
+  providers: [ArticleService]
 })
 export class ArticleDetailComponent implements OnInit {
+  articleId: string;
+  articleToDisplay;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private articleService: ArticleService
+  ) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParametersArray) =>{
+      this.articleId = urlParametersArray['id'];
+    });
+    this.articleService.getArtircleById(this.articleId).subscribe(dataLastEmittedFromObserver => {
+      this.articleToDisplay = new Article(dataLastEmittedFromObserver.title,
+                                          dataLastEmittedFromObserver.author,
+                                          dataLastEmittedFromObserver.text,
+                                          dataLastEmittedFromObserver.date);
+      console.log(this.articleToDisplay);
+    });
   }
-
 }
