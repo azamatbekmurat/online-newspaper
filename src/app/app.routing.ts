fa8a19b632f783781ebbc2b902cdda0393748b40
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2018-06-15 16:27:56 -0700

    add routing for listing news

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,32 @@
 import { ModuleWithProviders} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleDetailComponent } from './article-detail/article-detail.component';
+import { AdminComponent } from './admin/admin.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: WelcomeComponent
   },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'article-list',
+    component: ArticleListComponent
+  },
+  {
+    path: 'articles/:id',
+    component: ArticleDetailComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
