b34b7e682fbb56e13079df91cf8b14367a99be15
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2018-06-15 14:34:04 -0700

    update functionality in edit component

--- src/app/edit-article/edit-article.component.ts
+++ src/app/edit-article/edit-article.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ArticleService } from '../article.service';
 
 @Component({
   selector: 'app-edit-article',
   templateUrl: './edit-article.component.html',
-  styleUrls: ['./edit-article.component.css']
+  styleUrls: ['./edit-article.component.css'],
+  providers: [ArticleService]
 })
 export class EditArticleComponent implements OnInit {
+  @Input() selectedArticle;
 
-  constructor() { }
+  constructor(private articleService: ArticleService) { }
 
   ngOnInit() {
+  }
+
+  beginUpdatingArticle(articleToUpdate){
+    this.articleService.updateArticle(articleToUpdate);
+  }
+
+  beginDeletingArticle(articleToDelete){
+    if(confirm("Are you sure you want to delete?")){
+      this.articleService.deleteArticle(articleToDelete);
+    }
   }
 
 }
